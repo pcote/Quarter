fe1d89ef46bfafdba49b8bd46c1470ae2a032ac3
pcote <cotejrp@gmail.com>
2015-03-20 13:48:47 -0400

    Functional review and pep8 refactoring

--- quarter.py
+++ quarter.py
@@ -40,19 +40,19 @@ bl_info = {
 
 
 def get_mesh_data(rad=5, point_count=10, turn_width=.5, turn_height=0.0,
-                    points_per_turn=5, h_taper=0.0, w_taper=0.0):
-    
-    axis = [0,0,-1]
+                  points_per_turn=5, h_taper=0.0, w_taper=0.0):
+
+    axis = [0, 0, -1]
     cur_z = 0
     PI_2 = pi * 2
     ppt = points_per_turn-1
     num_of_turns = 0
-    point_count -= 1 #offset for the one point already in the new curve.
-    
+    point_count -= 1  # offset for the one point already in the new curve.
+
     x_vals = [x for x in range(1, point_count+2)]
     x_vals = [(x/ppt)*PI_2 for x in x_vals]
     quats = [Quaternion(axis, x) for x in x_vals]
-    
+
     def taper_values(turn_factor, taper):
         """Adjust heights or widths for tapering as needed"""
         new_list = []
@@ -60,20 +60,20 @@ def get_mesh_data(rad=5, point_count=10, turn_width=.5, turn_height=0.0,
             if i % points_per_turn == 0:
                 turn_factor -= taper
             new_list.append(turn_factor)
-                
-        return new_list        
-    
+
+        return new_list
+
     turn_heights = taper_values(turn_height, h_taper)
     turn_widths = taper_values(turn_width, w_taper)
-    
+
     vecs = []
     for i, q in enumerate(quats):
-        vec = q * Vector((rad,0,cur_z))
+        vec = q * Vector((rad, 0, cur_z))
         vecs.append(vec)
-        rad+=turn_widths[i]
-        cur_z+=turn_heights[i]
-        
-    coords = [(v.x,v.y,v.z) for v in vecs]
+        rad += turn_widths[i]
+        cur_z += turn_heights[i]
+
+    coords = [(v.x, v.y, v.z) for v in vecs]
     return coords
 
 
@@ -82,42 +82,48 @@ class AddCoilOperator(bpy.types.Operator):
     bl_idname = "curves.curve_coil_add"
     bl_label = "Add Curve Coil"
     bl_options = {'REGISTER', 'UNDO'}
-    
-    curve_choices = (('BEZIER', 'BEZIER', 'BEZIER'), 
-                      ('POLY', 'POLY', 'POLY'))
-    curve_type = EnumProperty( name="Curve Type", items=curve_choices,
-                     description="Choice of curve type: note yet implemented")
-                     
-    pc = IntProperty(
-        name = "Point Count", description = "Point Count",
-        min = 3, max = 50, default = 5)
-    radius = FloatProperty(
-        name = "Radius", description = "Radius",
-        min = .1, max = 10, default = 1)
+
+    curve_choices = (('BEZIER', 'BEZIER', 'BEZIER'),
+                     ('POLY', 'POLY', 'POLY'))
+
+    curve_type = EnumProperty(name="Curve Type", items=curve_choices,
+                              description="Choice of curve type: note yet implemented")
+
+    pc = IntProperty(name="Point Count", description="Point Count",
+                     min=3, max=50, default=5)
+
+    radius = FloatProperty(name="Radius", description="Radius",
+                           min=.1, max=10, default=1)
+
     turn_width = FloatProperty(name="Turn Width",
-                   min = -1.0, max=1.0, default=0)
+                               min=-1.0, max=1.0, default=0)
+
     turn_height = FloatProperty(name="Turn Height",
-                    min=-1.0, max=1.0,default=0)
+                                min=-1.0, max=1.0, default=0)
+
     points_per_turn = IntProperty(name="Points Per Turn",
-                    min=3,max=30,default=5)
-    h_taper = FloatProperty(name="Height Taper", 
-                        description="How much to decrease each turn height",
-                        min=-1, max=1,default=0)
-    w_taper = FloatProperty(name="Width Taper", 
-                        description="How much to decrease each turn height",
-                        min=-1, max=1,default=0)
+                                  min=3, max=30, default=5)
+
+    h_taper = FloatProperty(name="Height Taper",
+                            description="How much to decrease each turn height",
+                            min=-1, max=1, default=0)
+
+    w_taper = FloatProperty(name="Width Taper",
+                            description="How much to decrease each turn height",
+                            min=-1, max=1, default=0)
+
     bevel_depth = FloatProperty(name="Bevel Depth",
-                            description = "Amount of Bevel",
-                            min = 0, max = 1, default = 0)
+                                description="Amount of Bevel",
+                                min=0, max=1, default=0)
+
     extrude_mod = FloatProperty(name="Extrude",
-                            description = "Amount of Extrude",
-                            min = 0, max = 1, default = 0)
-    
+                                description="Amount of Extrude",
+                                min=0, max=1, default=0)
 
     def execute(self, context):
-        
+
         # set up the mesh data to be more suitable for curve objects.
-        mesh_data = get_mesh_data(rad=self.radius, 
+        mesh_data = get_mesh_data(rad=self.radius,
                                   point_count=self.pc,
                                   turn_width=self.turn_width,
                                   turn_height=self.turn_height,
@@ -128,7 +134,7 @@ class AddCoilOperator(bpy.types.Operator):
             flat_list.extend(md)
             if self.curve_type in ('POLY', 'NURBS'):
                 flat_list.append(0.0)
-            
+
         # build the curve
         crv = bpy.data.curves.new("crv", type="CURVE")
         spln = crv.splines.new(type=self.curve_type)
@@ -136,29 +142,27 @@ class AddCoilOperator(bpy.types.Operator):
             points = spln.points
         else:
             points = spln.bezier_points
-        
+
         if self.curve_type == 'BEZIER':
-            pass #set_trace()
+            pass
         points.add(self.pc-1)
         points.foreach_set("co", flat_list)
-        
+
         for i, point in enumerate(points):
             if i > 0 and hasattr(point, "handle_left_type"):
                 point.handle_left_type = point.handle_right_type = "AUTO"
-        
+
         crv.bevel_depth = self.bevel_depth
         crv.extrude = self.extrude_mod
-        ob = bpy.data.objects.new("quat_ob", crv)        
+        ob = bpy.data.objects.new("quat_ob", crv)
         bpy.context.scene.objects.link(ob)
-        
         return {'FINISHED'}
-        
-        
+
 
 def menu_func(self, context):
     self.layout.operator(AddCoilOperator.bl_idname,
-                            text="Add Coil Curve",
-                            icon = "PLUGIN")
+                         text="Add Coil Curve",
+                         icon="PLUGIN")
 
 
 def register():
@@ -170,6 +174,6 @@ def unregister():
     bpy.utils.unregister_class(AddCoilOperator)
     bpy.types.INFO_MT_mesh_add.remove(menu_func)
 
-            
+
 if __name__ == "__main__":
     register()
